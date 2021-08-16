de38ed9025778ab3c4e6efef1234ce0e2c8b2023
louisborn <louisborn12@gmail.com>
2021-08-16 17:01:28 +0200

    refactor: added setter functions

--- src/main/java/com/guard/restservice/Operator.java
+++ src/main/java/com/guard/restservice/Operator.java
@@ -47,4 +47,20 @@ public class Operator {
     public String getPassword() { return password; }
 
     public String getToken() { return token; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
