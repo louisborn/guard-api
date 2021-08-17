695e515daa109763dce54a5e3f43e6890793ad0a
louisborn <louisborn12@gmail.com>
2021-08-17 17:43:45 +0200

    refactor: deleted unused dependency

--- src/main/java/com/guard/restservice/tasks/TaskRepository.java
+++ src/main/java/com/guard/restservice/tasks/TaskRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
