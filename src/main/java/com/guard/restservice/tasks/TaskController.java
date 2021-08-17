052df84c4b9e5a99b81bc61154bd4050e80390ff
louisborn <louisborn12@gmail.com>
2021-08-17 17:26:53 +0200

    refactor: removed unused dependency

--- src/main/java/com/guard/restservice/tasks/TaskController.java
+++ src/main/java/com/guard/restservice/tasks/TaskController.java
@@ -1,7 +1,6 @@
 package com.guard.restservice.tasks;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
