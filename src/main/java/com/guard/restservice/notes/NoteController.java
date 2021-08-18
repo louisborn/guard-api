bf07e824f4376050606934fcdfa8b123b72265c2
louisborn <louisborn12@gmail.com>
2021-08-18 12:14:24 +0200

    refactor: added repository for notes

---
+++ src/main/java/com/guard/restservice/notes/NoteController.java
@@ -0,0 +1,2 @@
+package com.guard.restservice.notes;public class NoteController {
+}
---
+++ src/main/java/com/guard/restservice/notes/NoteRepository.java
@@ -0,0 +1,8 @@
+package com.guard.restservice.notes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
