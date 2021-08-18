2a1f690d4fe8c5e77b08113504471c4890430d75
louisborn <louisborn12@gmail.com>
2021-08-18 13:09:31 +0200

    refactor: added for debugging an example note to the repository

--- src/main/java/com/guard/restservice/GConfig.java
+++ src/main/java/com/guard/restservice/GConfig.java
@@ -1,5 +1,7 @@
 package com.guard.restservice;
 
+import com.guard.restservice.notes.Note;
+import com.guard.restservice.notes.NoteRepository;
 import com.guard.restservice.tasks.Task;
 import com.guard.restservice.tasks.TaskRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -12,7 +14,7 @@ import java.util.List;
 public class GConfig {
 
     @Bean
-    CommandLineRunner commandLineRunner(OperatorRepository operatorRepository, TaskRepository taskRepository) {
+    CommandLineRunner commandLineRunner(OperatorRepository operatorRepository, TaskRepository taskRepository, NoteRepository noteRepository) {
         return args -> {
             Operator john = new Operator("John Williams", "john.williams@bosch.de", "abc123", "");
 
@@ -21,9 +23,14 @@ public class GConfig {
             Task task2 = new Task(1, "Check B1" ,"Building B, ground floor", "10:32:22","2021-09-18",
                     "Please check B1 thoroughly once. Suspicion of intrusion.", false,true);
 
+            Note note1 = new Note("New employee starts today", "-", "Andre Müller starts today. Only duo patrols.",
+                    "John R. Williams", "05:00:45", "2021-09-18", true);
+
             operatorRepository.saveAll(List.of(john));
 
             taskRepository.saveAll(List.of(task1, task2));
+
+            noteRepository.saveAll(List.of(note1));
         };
     }
 }
