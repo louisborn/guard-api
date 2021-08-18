50a0a80f644ec96ef873a9319073766222f0d696
louisborn <louisborn12@gmail.com>
2021-08-18 12:51:59 +0200

    feat: added function to get all notes

--- src/main/java/com/guard/restservice/notes/NoteController.java
+++ src/main/java/com/guard/restservice/notes/NoteController.java
@@ -1,9 +1,26 @@
 package com.guard.restservice.notes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class NoteController {
 
+    private final NoteService noteService;
 
+    @Autowired
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping(path = "{token}/notes")
+    public List<Note> getNotes(
+            @PathVariable("token") String token
+    ) {
+        return noteService.getNotes(token);
+    }
 }
--- src/main/java/com/guard/restservice/notes/NoteService.java
+++ src/main/java/com/guard/restservice/notes/NoteService.java
@@ -1,7 +1,28 @@
 package com.guard.restservice.notes;
 
+import com.guard.restservice.OperatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NoteService {
+
+    private final NoteRepository noteRepository;
+    private final OperatorService operatorService;
+
+    @Autowired
+    public NoteService(NoteRepository noteRepository, OperatorService operatorService) {
+        this.noteRepository = noteRepository;
+        this.operatorService = operatorService;
+    }
+
+    public List<Note> getNotes(String token) {
+        if(!operatorService.checkTokenValidity(token)) {
+            throw new IllegalStateException("Access denied");
+        }
+        return noteRepository.findAll();
+    }
+
 }
