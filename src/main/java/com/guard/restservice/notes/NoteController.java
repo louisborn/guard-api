41198d01ffcce86115883150ff083d27bb643e3e
louisborn <louisborn12@gmail.com>
2021-08-18 12:15:17 +0200

    refactor: added rest controller for notes

--- src/main/java/com/guard/restservice/notes/NoteController.java
+++ src/main/java/com/guard/restservice/notes/NoteController.java
@@ -1,2 +1,9 @@
-package com.guard.restservice.notes;public class NoteController {
+package com.guard.restservice.notes;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoteController {
+
+
 }
