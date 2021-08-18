c2c692d0d1148418853c80c9f22160f8fb5364e9
louisborn <louisborn12@gmail.com>
2021-08-18 15:35:51 +0200

    feat: added function to calculate current local time and date

--- src/main/java/com/guard/restservice/GService.java
+++ src/main/java/com/guard/restservice/GService.java
@@ -1,2 +1,23 @@
-package com.guard.restservice;public class GService {
+package com.guard.restservice;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class GService {
+
+    public String calculateLocalTime() {
+        LocalTime localTime = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return localTime.format(formatter);
+    }
+
+    public String calculateLocalDate() {
+        LocalDate localDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return localDate.format(formatter);
+    }
 }
