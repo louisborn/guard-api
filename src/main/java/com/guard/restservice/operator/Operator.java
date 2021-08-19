8dbe677b0a2e7c2f0467132a1fb200637ae75a67
louisborn <louisborn12@gmail.com>
2021-08-19 14:20:04 +0200

    feat: added deviceId and applicationId to the operator model

--- src/main/java/com/guard/restservice/operator/Operator.java
+++ src/main/java/com/guard/restservice/operator/Operator.java
@@ -12,24 +12,30 @@ public class Operator {
     private String name;
     private String email;
     private String password;
+    private String deviceId;
     private String token;
+    private String applicationId;
 
     public Operator() {
     }
 
-    public Operator(long id, String name, String email, String password, String token) {
+    public Operator(long id, String name, String email, String password, String deviceId, String token, String applicationId) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.deviceId = deviceId;
         this.token = token;
+        this.applicationId = applicationId;
     }
 
-    public Operator(String name, String email, String password, String token) {
+    public Operator(String name, String email, String password, String deviceId, String token, String applicationId) {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.deviceId = deviceId;
         this.token = token;
+        this.applicationId = applicationId;
     }
 
     public long getId() {
@@ -46,7 +52,15 @@ public class Operator {
 
     public String getPassword() { return password; }
 
+    public String getDeviceId() {
+        return deviceId;
+    }
+
     public String getToken() { return token; }
+
+    public String getApplicationId() {
+        return applicationId;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -60,7 +74,15 @@ public class Operator {
         this.password = password;
     }
 
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public void setApplicationId(String applicationId) {
+        this.applicationId = applicationId;
     }
 }
