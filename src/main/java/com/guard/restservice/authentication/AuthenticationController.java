4a067a43fc87a9833cf2a2b048876da3251d7956
louisborn <louisborn12@gmail.com>
2021-08-19 15:21:18 +0200

    feat: registered operators can authenticate over token

--- src/main/java/com/guard/restservice/authentication/AuthenticationController.java
+++ src/main/java/com/guard/restservice/authentication/AuthenticationController.java
@@ -1,2 +1,45 @@
-package com.guard.restservice.authentication;public class AuthenticationController {
+package com.guard.restservice.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @GetMapping(path = "handshake")
+    public Map<String, String> getHandshake(
+            @RequestHeader(name = "X-DEVICE-ID") String deviceId,
+            @RequestHeader(name = "X-TOKEN") String token,
+            @RequestHeader(name = "X-APP-ID") String applicationId
+    ) {
+        authenticationService.validateHandshake(deviceId, token, applicationId);
+        Map<String, String> response = new HashMap<>();
+        switch (authenticationService.getTokenStatus()) {
+            case VALID:
+                response.put("status", "VALID");
+                break;
+            case INVALID:
+                response.put("status", "INVALID");
+                break;
+            case EXPIRED:
+                response.put("status", "EXPIRED");
+                break;
+            case REGISTRATION:
+                response.put("status", "REGISTRATION");
+                break;
+        }
+        return response;
+    }
 }
--- src/main/java/com/guard/restservice/authentication/AuthenticationService.java
+++ src/main/java/com/guard/restservice/authentication/AuthenticationService.java
@@ -1,15 +1,102 @@
 package com.guard.restservice.authentication;
 
+import com.guard.restservice.operator.Operator;
+import com.guard.restservice.operator.OperatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Optional;
+
+enum TokenStatus {
+    INVALID,
+    EXPIRED,
+    VALID,
+    REGISTRATION,
+}
 
 @Service
-public class TokenAuthentication {
+public class AuthenticationService {
 
-    @GetMapping(path = "handshake")
-    public Map<String, Boolean> validateHandshake() {
+    private final OperatorService operatorService;
 
+    private String token;
+
+    private TokenStatus tokenStatus;
+
+    public TokenStatus getTokenStatus() {
+        return tokenStatus;
+    }
+
+    public void setTokenStatus(TokenStatus tokenStatus) {
+        this.tokenStatus = tokenStatus;
+    }
+
+    private String deviceId;
+    private String applicationId;
+    private String operatorEmail;
+    private LocalDate expirationDate;
+
+    @Autowired
+    public AuthenticationService(OperatorService operatorService) {
+        this.operatorService = operatorService;
+    }
+
+    public void validateHandshake(String deviceId, String token, String applicationId) {
+        if(deviceId.isEmpty()) {
+            tokenStatus = TokenStatus.INVALID;
+        }
+        Optional<Operator> operator = operatorService.getOperatorByDeviceId(deviceId);
+        if(!operator.isPresent()) {
+            tokenStatus = TokenStatus.REGISTRATION;
+        }
+        if(applicationId.isEmpty() || !applicationId.equals(operator.get().getApplicationId())) {
+            tokenStatus = TokenStatus.INVALID;
+        }
+        this.deviceId = deviceId;
+        this.applicationId = applicationId;
+        this.operatorEmail = operator.get().getEmail();
+        if(token.isEmpty()) {
+            generateToken();
+        }
+        validateToken(token);
+    }
+
+    public void validateToken(String token) {
+        try {
+            byte[] bytes = Base64.getDecoder().decode(token);
+            String decrypted = new String(bytes);
+
+            String[] parts = decrypted.split("~", -2);
+
+            if(!deviceId.equals(parts[0])) {
+                tokenStatus = TokenStatus.INVALID;
+            }
+            if(!applicationId.equals(parts[1])) {
+                tokenStatus = TokenStatus.INVALID;
+            }
+            if(!operatorEmail.equals(parts[2])) {
+                tokenStatus = TokenStatus.INVALID;
+            }
+
+            int span = LocalDate.now().getDayOfMonth() - expirationDate.getDayOfMonth();
+            if(span > 0 ) {
+                tokenStatus = TokenStatus.EXPIRED;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void generateToken() {
+        this.expirationDate = LocalDate.now().plus(1, ChronoUnit.DAYS);
+
+        //{deviceId}~{applicationId}~{operatorEmail}~{expirationDate}
+        String tokenInClear = deviceId + "~" + applicationId + "~" + operatorEmail + "~" + expirationDate;
+
+        this.token = Base64.getEncoder().encodeToString(tokenInClear.getBytes(StandardCharsets.UTF_8));
     }
 }
